b8a7dd0252953449166a1fcfbdf2356fac325789
quintin <quintin@quintin-desktop>
2015-03-21 13:51:21 -0400

    Added getTotal and getSolutions methods as well as prompts for the user.

--- lab7/Lab7/src/lab7/Lab7.java
+++ lab7/Lab7/src/lab7/Lab7.java
@@ -1,21 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab7;
 
+import java.util.ArrayList;
+
+/*
+* Quintin Oliver
+* Section 09
+* TA: Jacob Ross
+* Lecture: Michael Ondrasek
+*/
+
 /**
- *
- * @author quintin
+ * Finds a set of coins to make change for a user entered amount of currency
+ * using the backtracking algorithm.
  */
 public class Lab7 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    // Create an ArrayList that will hold all of the solutions
+    static ArrayList<ArrayList<Integer>> solutions = new ArrayList<>();
     
-}
+    public static void main(String[] args) {
+        DataInput keyboard = new DataInput();
+        ArrayList<Integer> coins = new ArrayList<>();
+        int numOfCoins;
+        int total;
+        ArrayList<Integer> partialSolution = new ArrayList<>();
+        
+        // Prompt the user for the amount of change
+        System.out.print("Enter amount: " );
+        total = keyboard.getNextInt();
+        
+        // Prompt user for the number of coins
+        System.out.print("Enter number of coins: ");
+        numOfCoins = keyboard.getNextInt();
+        
+        // Prompt the user for the coin values
+        System.out.print("Enter coin values: ");
+        for(int i = 0; i < numOfCoins; i++){
+            coins.add(keyboard.getNextInt());
+        }//end for
+        coins.sort(null);
+        
+        // Get the combinations
+        getSolutions(partialSolution, coins, total);
+    }//end main
+    
+    
+    public static void getSolutions(ArrayList<Integer> partialSolution, 
+            ArrayList<Integer> coins, int total){
+        // Get current partial solution totale
+        int partialTotal = getTotal(partialSolution);
+        // Case partial solution = total
+        if(partialTotal == total){
+            solutions.add(partialSolution);
+        }else{
+            int i = coins.size();
+        }
+    }//end getSolution
+    
+    public static int getTotal(ArrayList<Integer> nums){
+        int total = 0;
+        for(int num : nums){
+            total += num;
+        }
+        return total;
+    }
+}//end class
