b04bd95a372b13d07866602dffa546ea13cd2381
quintin <quintin@quintin-desktop>
2015-03-21 09:37:57 -0400

    Initial Commit

---
+++ proj4/Lab7Part2/src/lab7part2/Lab7Part2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7part2;
+
+/**
+ *
+ * @author quintin
+ */
+public class Lab7Part2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
