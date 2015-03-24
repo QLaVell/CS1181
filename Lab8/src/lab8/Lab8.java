49f88fdac56d1c535c60face5f50d1340bd0bfe3
student <student@130.108.7.137>
2015-03-24 16:31:49 -0400

    Fixed sort and started search method

--- Lab8/src/lab8/Lab8.java
+++ Lab8/src/lab8/Lab8.java
@@ -1,8 +1,5 @@
 package lab8;
 
-import java.util.ArrayList;
-
-
 /*
 * Quintin Oliver
 * Section 09
@@ -20,79 +17,64 @@ import java.util.ArrayList;
 public class Lab8 {
 
     public static void main(String[] args) {
-        double[] values = new double[10];
-        double[] temp = new double[10];
+        final int SIZE = 1000;
+        double[] values = new double[SIZE];
         
         //Fill the array with values
         for(int i = 0; i < values.length; i++){
             values[i] = Math.random() * 10;
         }
         
-        System.out.print("Pre:");
+        System.out.println("Pre:");
         //Print pre-sort
         for(Double num : values){
-            System.out.print(num + " ");
+            System.out.println(num);
         }
         System.out.println("");
         
         // Sort the array using merge sort
-        sortArray(values, temp, 0, values.length - 1);
+        sortArray(values, 0, values.length - 1);
         
         System.out.println("Post:");
         //Print post-sort
         for(Double num : values){
-            System.out.print(num + " ");
+            System.out.println(num);
         }
         System.out.println("");
     }
     
-    public static void sortArray(double[] values, 
-            double[] tempArray, int first, int last){
-        if(first < last){
-            // Get the middle item. In case of even array middle is left side
-            int mid = first + (last - first)/2;
-            sortArray(values, tempArray, first, mid);
-            sortArray(values, tempArray, mid + 1, last);
-            merge(values, tempArray, first, mid, last);
-        }
-        
+    public static void sortArray(double[] values, int first, int last){
+        insertionSort(values, first, last);
     }
     
-    private static void merge(double[] values, 
-            double[] tempArray, int first, int mid, int last){
-        int firstHalfBegin = first;
-        int firstHalfEnd = mid;
-        int secondHalfBegin = mid + 1;
-        int secondHalfEnd = last;
-        
-        int index = 0;
-        
-        while((firstHalfBegin <= firstHalfEnd) && (secondHalfBegin <= secondHalfEnd)){
-            if(values[firstHalfBegin] <= values[secondHalfBegin]){
-                tempArray[index] = values[firstHalfBegin];
-                firstHalfBegin++;
-            }else{
-                tempArray[index] = values[secondHalfBegin];
-                secondHalfBegin++;
-            }//end if-else
-            index++;
-        }//end while
-        
-        while(firstHalfBegin < firstHalfEnd){
-            tempArray[index] = values[firstHalfBegin];
-            firstHalfBegin++;
-            index++;
-        }//end while
-        
-        while(secondHalfBegin < secondHalfEnd){
-            tempArray[index] = values[secondHalfBegin];
-            secondHalfBegin++;
-            index++;
-        }//end while
-        
-        for(int i = first; i <= last; i++){
-            values[i] = tempArray[i];
-        }//end for
+    private static void insertionSort(double[] values, int first, int last){
+        if(first < last){
+            insertionSort(values, first, last - 1);
+            
+            insertInOrder(values[last], values, first, last -1);
+        }
     }
-
+    
+    private static void insertInOrder(double val, double[] values, int begin, int end){
+        if(val >= values[end]){
+            values[end + 1] = val;
+        }else if(begin < end){
+            values[end + 1] = values[end];
+            insertInOrder(val, values, begin, end - 1);
+        }else{
+            values[end + 1] = values[end];
+            values[end] = val;
+        }
+    }
+    
+    private boolean interpolationSearch(int lowerBound, int upperBound, 
+            double[] values, double searchValue){
+        boolean success = false;
+        
+        double lowerValue = values[lowerBound];
+        double upperValue = values[upperBound];
+        int searchPos;
+        
+        return success;
+    }
 }
